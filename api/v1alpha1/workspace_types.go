ed0eb96dab9fc80c467a5aa64dfb718c4821aea5
guofei <guofei@microsoft.com>
2023-07-27 23:56:17 -0700

    Add CRD definitions

--- api/v1alpha1/workspace_types.go
+++ api/v1alpha1/workspace_types.go
@@ -17,25 +17,61 @@ limitations under the License.
 package v1alpha1
 
 import (
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+type ResourceSpec struct {
+	// The number of required GPU nodes.
+	Count *int `json:"count,omitempty"`
 
-// WorkspaceSpec defines the desired state of Workspace
-type WorkspaceSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// The required instance type of the GPU node.
+	InstanceType string `json:"instanceType,omitempty"`
 
-	// Foo is an example field of Workspace. Edit workspace_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// The required label for the GPU node.
+	LabelSelector *metav1.LabelSelector `json:"labelSelector,omitempty"`
+
+	// The existing GPU nodes with the required labels and the required instancetype.
+	// This field is used when the number of qualified existing nodes is larger than the required count.
+	// Users need to ensure supported VHD images are installed in the VMs.
+	Nodes []string `json:"nodes,omitempty"`
+}
+
+type PresetModelName string
+
+const (
+	PresetSetModelllama2A            PresetModelName = "llama2-7b"
+	PresetSetModelllama2B            PresetModelName = "llama2-13b"
+	PresetSetModelllama2C            PresetModelName = "llama2-70b"
+	PresetSetModelStableDiffusionXXX PresetModelName = "stablediffusion-xxx"
+)
+
+type PresetModelSpec struct {
+	// Name of a supported preset model, e.g., llama2-7b.
+	Name PresetModelName `json:"name,omitempty"`
+	// The custom volume that will be mounted to the pod running preset models.
+	// Later, we may limit to AzureFile and configmap in API.
+	Volume []v1.Volume `json:"volume,omitempty"`
+}
+
+type InferenceSpec struct {
+	// The preset model to be deployed.
+	Preset PresetModelSpec `json:"preset,omitempty"`
+	// The Pod template used by the Deployment. Users can use custom image and Pod spec.
+	// Leave this filed unset if preset model is used.
+	Template v1.PodTemplateSpec `json:"template,omitempty"`
+}
+
+type TrainingSpec struct {
+	// Job pytorchJob `json:"job,omitempty"`
 }
 
 // WorkspaceStatus defines the observed state of Workspace
 type WorkspaceStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	WorkerNodes    []string `json:"workernodes,omitempty"`
+	ResourceStatus string   `json:"resourceStatus,omitempty"`
+	// Determined by the status of the created deployment1
+	WorkloadStatus string `json:"workloadStatus,omitempty"`
 }
 
 //+kubebuilder:object:root=true
@@ -46,7 +82,10 @@ type Workspace struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   WorkspaceSpec   `json:"spec,omitempty"`
+	Resource  ResourceSpec  `json:"resource,omitempty"`
+	Inference InferenceSpec `json:"inference,omitempty"`
+	Training  TrainingSpec  `json:"training,omitempty"`
+
 	Status WorkspaceStatus `json:"status,omitempty"`
 }
 
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/onsi/ginkgo/v2 v2.9.5
 	github.com/onsi/gomega v1.27.7
+	k8s.io/api v0.27.2
 	k8s.io/apimachinery v0.27.2
 	k8s.io/client-go v0.27.2
 	sigs.k8s.io/controller-runtime v0.15.0
@@ -61,7 +62,6 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.27.2 // indirect
 	k8s.io/apiextensions-apiserver v0.27.2 // indirect
 	k8s.io/component-base v0.27.2 // indirect
 	k8s.io/klog/v2 v2.90.1 // indirect
