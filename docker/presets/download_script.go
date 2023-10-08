00a5db717f254c45c8356473018100c1b757c337
ishaansehgal99 <ishaanforthewin@gmail.com>
2023-10-07 22:40:32 -0700

    fix: falcon naming and cmd line params for download script

--- docker/presets/download_script.go
+++ docker/presets/download_script.go
@@ -194,6 +194,9 @@ func main() {
 	token := ""
 	baseURL := ""
 	if linkType == PrivateLink {
+		if len(os.Args) != 4 {
+			log.Fatalf("Usage (private link): %s <link_type> <model_version> <external_IP> <external_port>", os.Args[0])
+		}
 		token = os.Getenv("AUTH_TOKEN_ENV_VAR")
 		if token == "" {
 			log.Fatal("AUTH_TOKEN_ENV_VAR not set!")
