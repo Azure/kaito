17a47bdc536711b46953073160c6a589b188513b
ishaansehgal99 <ishaanforthewin@gmail.com>
2023-12-04 13:09:37 -0800

    feat: update base image to include onnx dependencies

--- docker/presets/tfs/convert_to_onnx.py
+++ docker/presets/tfs/convert_to_onnx.py
@@ -1,68 +1,73 @@
 import sys
 import subprocess
-from huggingface_hub import snapshot_download
 from optimum.onnxruntime import AutoOptimizationConfig, ORTModelForCausalLM, ORTOptimizer
+import glob
 
-def download_and_convert_optimum_cli(repo_name): 
-    # First try downloading from HF
-    try:
-        # Construct the optimum-cli command
-        command = [
-            "optimum-cli", "export", "onnx",
-            "--model", repo_name, repo_name,
-            "--task", "text-generation", "--framework", "pt",
-            "--library", "transformers",
-            "--cache_dir", repo_name
-        ]
+def get_model_name(repo_name): 
+    last_slash_idx= repo_name.rfind('/')
+    model_name = repo_name[last_slash_index + 1:] if last_slash_index != -1 else repo_name
+    return model_name
 
-        subprocess.run(command, check=True)
-        model = ORTModelForCausalLM.from_pretrained(repo_name, use_cache=False, use_io_binding=False)
-    except Exception as e:
-        try: 
-            # Otherwise Manually download model
-            download_path = snapshot_download(repo_id=repo_name)
-
-            command = [
-                "optimum-cli", "export", "onnx",
-                "--model", download_path, repo_name,
-                "--task", "text-generation", "--framework", "pt",
-                "--library", "transformers",
-                "--cache_dir", download_path
-            ]
-
-            subprocess.run(command, check=True)
-            model = ORTModelForCausalLM.from_pretrained(repo_name, use_cache=False, use_io_binding=False, local_files_only=True)
-        except Exception as e:
-            print("Failed to convert model to ONNX using Optmum CLI", e)
-            return None
-    return model
+def download_and_convert(repo_name, model_name):
+    """
+    Download and convert a model to ONNX format.
     
-def download_and_convert(repo_name):
-    # Try converting to ONNX first with caching, if fails, retry with disabled caching mechanism
-    # export=True flag specifies converting from pytorch to ONNX format
-    try:
-        model = ORTModelForCausalLM().from_pretrained(repo_name, export=True, provider="CUDAExecutionProvider")
-    except Exception as e:
-        try:
-            model = ORTModelForCausalLM.from_pretrained(repo_name, use_cache=False, export=True, provider="CUDAExecutionProvider")
-        # Lastly try using CLI
-        except Exception as e: 
-            print(f"Failed to load model with provider {provider}", e)
-            model = download_and_convert_optimum_cli(repo_name)
-    return model
+    Parameters:
+    repo_name (str): The repository name to download the model from.
+    model_name (str): The name to save the ONNX model as.
 
-def onnx_optimize_model(repo_name, model):
+    Returns:
+    ORTModelForCausalLM: The loaded or converted ONNX model, or None if failed.
+    """
+    if not repo_name or not model_name:
+        print("Repository name and model name must be provided")
+        return None
+
+    # Search for .onnx files in the current and subdirectories
+    onnx_files = glob.glob('**/*.onnx', recursive=True)
+
+    # If an ONNX file is found, use it
+    if onnx_files:
+        file_path = onnx_files[0]
+        try:
+            model = ORTModelForCausalLM.from_pretrained(file_path)
+            print(f"Loaded local ONNX model from {file_path}")
+            return model
+        except Exception as e:
+            print(f"Loading local ONNX model from {file_path} failed: {e}")
+
+    # Try converting to ONNX with caching, then without if fails
+    try:
+        model = ORTModelForCausalLM.from_pretrained(repo_name, export=True, provider="CUDAExecutionProvider")
+        model.save_pretrained(f"{model_name}.onnx")
+        print(f"Model converted and saved as {model_name}.onnx")
+        return model
+    except Exception as e:
+        print(f"Failed to convert model to ONNX with caching: {e}")
+    
+    try:
+        model = ORTModelForCausalLM.from_pretrained(repo_name, use_cache=False, export=True, provider="CUDAExecutionProvider")
+        model.save_pretrained(f"{model_name}.onnx")
+        print(f"Model converted without cache and saved as {model_name}.onnx")
+        return model
+    except Exception as e:
+        print(f"Failed to convert model from {repo_name} without caching: {e}")
+    return None
+
+def onnx_optimize_model(model, model_name):
     try:
         optimizer = ORTOptimizer.from_pretrained(model)
         optimization_config = AutoOptimizationConfig.O2()
-        optimizer.optimize(save_dir=repo_name, optimization_config=optimization_config)
+        optimizer.optimize(save_dir=model_name, optimization_config=optimization_config)
     except NotImplementedError as e: 
-        print("ONNX Runtime not supported for this model yet:", e)
+        print("ONNX Optimization not supported for this model yet:", e)
     except Exception as e: 
         print("Optimizing model failed", e)
 
 if __name__ == "__main__":
     repo_name = sys.argv[1]
-    model = download_and_convert(repo_name)
-    onnx_optimize_model(repo_name, model)
+    model_name = get_model_name(repo_name)
+    model = download_and_convert(repo_name, model_name)
+    if model: 
+        onnx_optimize_model(model, model_name)
 
