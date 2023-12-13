1e33923fc5209e375126711160726c77f2adecb6
ishaansehgal99 <ishaanforthewin@gmail.com>
2023-12-13 14:22:37 -0800

    feat: restructured dockerfile locations, updated docker job template to include new dockerfile path and weights path

--- .github/workflows/init-pod/main.py
+++ .github/workflows/init-pod/main.py
@@ -4,6 +4,72 @@ import shutil
 
 KAITO_REPO_URL = "https://github.com/Azure/kaito.git"
 
+HOST_WEIGHTS_PATHS = {
+    # TFS Weights
+    "falcon-7b": "/home/tfs/tiiuae/falcon-7b/weights",
+    "falcon-7b-instruct": "/home/tfs/tiiuae/falcon-7b-instruct/weights",
+    "falcon-40b": "/home/tfs/tiiuae/falcon-40b/weights",
+    "falcon-40b-instruct": "/home/tfs/tiiuae/falcon-40b-instruct/weights",
+    "mistral-7b-v01": "/home/tfs/mistralai/mistral-7b-v0.1/weights",
+    "mistral-7b-instruct-v0.1": "/home/tfs/mistralai/mistral-7b-instruct-v0.1/weights",
+
+    # TFS Onnx Weights
+    "falcon-7b-instruct-onnx": "/home/tfs/tiiuae/falcon-7b-instruct-onnx/weights",
+
+    # Llama Weights (Mounted on /datadrive drive)
+    "llama-2-7b": "/datadrive/llama/llama-2-7b",
+    "llama-2-7b-chat": "/datadrive/llama/llama-2-7b-chat",
+    "llama-2-13b": "/datadrive/llama/llama-2-13b",
+    "llama-2-13b-chat": "/datadrive/llama/llama-2-13b-chat",
+    "llama-2-70b": "/datadrive/llama/llama-2-70b",
+    "llama-2-70b-chat": "/datadrive/llama/llama-2-70b-chat"
+}
+
+REPO_PRESET_PATHS = {
+    # Falcon Presets
+    "falcon-7b": "presets/models/falcon",
+    "falcon-7b-instruct": "presets/models/falcon",
+    "falcon-40b": "presets/models/falcon",
+    "falcon-40b-instruct": "presets/models/falcon",
+
+    # Mistral Presets
+    "mistral-7b-v01": "presets/models/mistral",
+    "mistral-7b-instruct-v0.1": "presets/models/mistral",
+
+    # TFS Onnx Presets
+    "falcon-7b-instruct-onnx": "presets/models/falcon",
+
+    # Llama Presets
+    "llama-2-7b": "presets/models/llama2",
+    "llama-2-7b-chat": "presets/models/llama2chat",
+    "llama-2-13b": "presets/models/llama2",
+    "llama-2-13b-chat": "presets/models/llama2chat",
+    "llama-2-70b": "presets/models/llama2",
+    "llama-2-70b-chat": "presets/models/llama2chat"
+}
+
+
+MODEL_TYPE = {
+    # TFS Types
+    "falcon-7b": "tfs",
+    "falcon-7b-instruct": "tfs",
+    "falcon-40b": "tfs",
+    "falcon-40b-instruct": "tfs",
+    "mistral-7b-v01": "tfs",
+    "mistral-7b-instruct-v0.1": "tfs",
+
+    # TFS Onnx Presets
+    "falcon-7b-instruct-onnx": "tfs-onnx",
+
+    # Llama Presets
+    "llama-2-7b": "llama-2",
+    "llama-2-7b-chat": "llama-2",
+    "llama-2-13b": "llama-2",
+    "llama-2-13b-chat": "llama-2",
+    "llama-2-70b": "llama-2",
+    "llama-2-70b-chat": "llama-2"
+}
+
 def run_command(command):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     output, error = process.communicate()
@@ -18,21 +84,22 @@ def main():
     img_tag = os.environ.get("IMAGE_TAG", "0.0.1")
     print(f"image_tag: {img_tag}")
 
-    mod_files = check_modified_files(pr_branch)
-    print("Modified files", mod_files)
+    mod_models = check_modified_models(pr_branch)
+    print("Modified files", mod_models)
 
-    run_build_pods(mod_files, img_tag)
+    run_build_pods(pr_branch, img_tag, mod_models)
 
-def run_build_pods(mod_files, img_tag):
+def run_build_pods(pr_branch, img_tag, mod_models):
     acr_name = os.environ.get("ACR_NAME", "aimodelsregistrytest")
     acr_username = os.environ.get("ACR_USERNAME")
     acr_password = os.environ.get("ACR_PASSWORD")
 
-    for key, modified in mod_files.items(): 
+    for model, modified in mod_models.items(): 
         if modified:
-            image_name = key.replace("_modified", "").replace("_", "-")
+            image_name = model
+            model_type = MODEL_TYPE[model]
 
-            job_yaml = populate_job_template(image_name, img_tag, acr_name, acr_username, acr_password)
+            job_yaml = populate_job_template(image_name, img_tag, acr_name, acr_username, acr_password, pr_branch, model_type)
 
             with open(f"{image_name}-job.yaml", "w") as file: 
                 file.write(job_yaml)
@@ -40,7 +107,7 @@ def run_build_pods(mod_files, img_tag):
             run_command(f"kubectl apply -f {image_name}-job.yaml")
 
 
-def populate_job_template(image_name, img_tag, acr_name, acr_username, acr_password):
+def populate_job_template(image_name, img_tag, acr_name, acr_username, acr_password, pr_branch, model_type):
     with open("docker-job-template.yaml", "r") as file:
         job_template = file.read()
 
@@ -51,10 +118,13 @@ def populate_job_template(image_name, img_tag, acr_name, acr_username, acr_passw
     job_template = job_template.replace("{{ACR_NAME}}", acr_name)
     job_template = job_template.replace("{{ACR_USERNAME}}", acr_username)
     job_template = job_template.replace("{{ACR_PASSWORD}}", acr_password)
+    job_template = job_template.replace("{{PR_BRANCH}}", pr_branch)
+    job_template = job_template.replace("{{MODEL_TYPE}}", model_type)
+    job_template = job_template.replace("{{HOST_WEIGHTS_PATH}}", HOST_WEIGHTS_PATHS[image_name])
 
     return job_template
 
-def check_modified_files(pr_branch):
+def check_modified_models(pr_branch):
     repo_dir = "repo"
     repo_path = os.path.join(os.getcwd(), repo_dir)
 
@@ -82,27 +152,14 @@ def check_modified_files(pr_branch):
 
     print("Modified files:", files)
 
-    # Paths to check
-    paths_to_check = {
-        "falcon_7b_modified": "presets/falcon-7b/",
-        "falcon_7b_chat_modified": "presets/falcon-7b-chat",
-        "falcon_7b_chat_onnx_modified": "presets/falcon-7b-chat-onnx",
-        "falcon_40b": "presets/falcon-40b",
-        "falcon_40b_chat": "presets/falcon-40b-chat",
-        "mistral_7b_v01": "presets/mistral-7b-v0.1",
-        "mistral_7b_instruct_v01": "presets/mistral-7b-instruct-v0.1",
-        "llama2_modified": "presets/llama-2/",
-        "llama2_chat_modified": "presets/llama-2-chat/"
-    }
-
-    # Check modified files against paths
-    modified_files = {key: path in files for key, path in paths_to_check.items()}
+    # Check modified models against paths
+    modified_models = {model: preset_path in files for model, preset_path in REPO_PRESET_PATHS.items()}
 
     # Print modified status
-    for key, modified in modified_files.items():
+    for key, modified in modified_models.items():
         print(f"{key}: {modified}")
 
-    return modified_files
+    return modified_models
 
 
 if __name__ == "__main__":
