7f6afad8e94e69fa6c85a07e79849c012ca09a22
ishaansehgal99 <ishaanforthewin@gmail.com>
2023-12-19 16:41:06 -0700

    nit: add comment

--- pkg/inference/preset-inferences.go
+++ pkg/inference/preset-inferences.go
@@ -116,6 +116,10 @@ func CreatePresetInference(ctx context.Context, workspaceObj *kaitov1alpha1.Work
 	return depObj, nil
 }
 
+// prepareInferenceParameters builds a PyTorch command:
+// torchrun <TORCH_PARAMS> <RDZV_PARAMS> baseCommand <MODEL_PARAMS>
+// and sets the GPU resources required for inference.
+// Returns the command and resource configuration.
 func prepareInferenceParameters(ctx context.Context, inferenceObj *model.PresetInferenceParam) ([]string, corev1.ResourceRequirements) {
 	torchCommand := buildCommandStr(inferenceObj.BaseCommand, inferenceObj.TorchRunParams)
 	torchCommand = buildCommandStr(torchCommand, inferenceObj.TorchRunRdzvParams)
