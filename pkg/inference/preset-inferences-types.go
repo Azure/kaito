7fb5166b46acac12800558d460d68aa92173cb7a
ishaansehgal99 <ishaanforthewin@gmail.com>
2024-01-03 14:55:19 -0800

    fix: add defaults

---
+++ pkg/inference/preset-inferences-types.go
@@ -0,0 +1,60 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+package inference
+
+import (
+	corev1 "k8s.io/api/core/v1"
+)
+
+const (
+	DefaultNnodes       = "1"
+	DefaultNprocPerNode = "1"
+	DefaultNodeRank     = "0"
+	DefaultMasterAddr   = "localhost"
+	DefaultMasterPort   = "29500"
+)
+
+// Torch Rendezvous Params
+const (
+	DefaultMaxRestarts  = "3"
+	DefaultRdzvId       = "rdzv_id"
+	DefaultRdzvBackend  = "c10d"            // Pytorch Native Distributed data store
+	DefaultRdzvEndpoint = "localhost:29500" // e.g. llama-2-13b-chat-0.llama-headless.default.svc.cluster.local:29500
+)
+
+const (
+	DefaultConfigFile   = "config.yaml"
+	DefaultNumProcesses = "1"
+	DefaultNumMachines  = "1"
+	DefaultMachineRank  = "0"
+	DefaultGPUIds       = "all"
+)
+
+// TODO: remove the above local variables starting with lower cases.
+var (
+	DefaultTorchRunParams = map[string]string{
+		"nnodes":         DefaultNnodes,
+		"nproc_per_node": DefaultNprocPerNode,
+		"node_rank":      DefaultNodeRank,
+		"master_addr":    DefaultMasterAddr,
+		"master_port":    DefaultMasterPort,
+	}
+
+	DefaultTorchRunRdzvParams = map[string]string{
+		"max_restarts":  DefaultMaxRestarts,
+		"rdzv_id":       DefaultRdzvId,
+		"rdzv_backend":  DefaultRdzvBackend,
+		"rdzv_endpoint": DefaultRdzvEndpoint,
+	}
+
+	DefaultAccelerateParams = map[string]string{
+		"config_file":   DefaultConfigFile,
+		"num_processes": DefaultNumProcesses,
+		"num_machines":  DefaultNumMachines,
+		"machine_rank":  DefaultMachineRank,
+		"gpu_ids":       DefaultGPUIds,
+	}
+
+	DefaultAccessMode       = "public"
+	DefaultImagePullSecrets = []corev1.LocalObjectReference{}
+)
