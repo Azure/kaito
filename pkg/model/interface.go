e0dd658c98c977d2ae63cdb26ba899173e07e4a1
Ishaan Sehgal <ishaanforthewin@gmail.com>
2024-01-03 23:01:42 -0600

    chore: Commenting and Renaming (#194)
    rename modelname -> modelfamilyname
    accessmode -> ImageAccessMode
    Add comments to interface.go and preset-inferences.go

--- pkg/model/interface.go
+++ pkg/model/interface.go
@@ -13,26 +13,25 @@ type Model interface {
 	SupportDistributedInference() bool //If true, the model workload will be a StatefulSet, using the torch elastic runtime framework.
 }
 
-// PresetInferenceParam defines the preset inference.
+// PresetInferenceParam defines the preset inference parameters for a model.
 type PresetInferenceParam struct {
-	ModelName                 string
-	Image                     string
-	ImagePullSecrets          []corev1.LocalObjectReference
-	AccessMode                string
-	DiskStorageRequirement    string
-	GPUCountRequirement       string
-	TotalGPUMemoryRequirement string
-	PerGPUMemoryRequirement   string
-	TorchRunParams            map[string]string
-	TorchRunRdzvParams        map[string]string
-	ModelRunParams            map[string]string
-	InferenceFile             string
+	ModelFamilyName           string                        // The name of the model family.
+	Image                     string                        // Docker image used for running the inference.
+	ImagePullSecrets          []corev1.LocalObjectReference // Secrets for pulling the image from a private registry.
+	ImageAccessMode           string                        // Defines where the Image is Public or Private.
+	DiskStorageRequirement    string                        // Disk storage requirements for the model.
+	GPUCountRequirement       string                        // Number of GPUs required for the inference.
+	TotalGPUMemoryRequirement string                        // Total GPU memory required for the inference.
+	PerGPUMemoryRequirement   string                        // GPU memory required per GPU.
+	TorchRunParams            map[string]string             // Parameters for configuring the torchrun command.
+	TorchRunRdzvParams        map[string]string             // Optional rendezvous parameters for distributed inference using torchrun (elastic).
+	ModelRunParams            map[string]string             // Parameters for running the model inference.
 	// DeploymentTimeout defines the maximum duration for pulling the Preset image.
-	// This timeout accommodates the size of PresetX, ensuring pull completion
+	// This timeout accommodates the size of the image, ensuring pull completion
 	// even under slower network conditions or unforeseen delays.
 	DeploymentTimeout time.Duration
-	BaseCommand       string
-	// WorldSize defines num of processes required for inference
+	// BaseCommand is the initial command (e.g., 'torchrun', 'accelerate launch') used in the command line.
+	BaseCommand string
+	// WorldSize defines the number of processes required for distributed inference.
 	WorldSize              int
-	DefaultVolumeMountPath string
 }
