e6059c421917500944dc35e274d5714c34fa200b
ishaansehgal99 <ishaanforthewin@gmail.com>
2024-01-17 13:39:20 -0800

    fix: include test

--- pkg/utils/testModel.go
+++ pkg/utils/testModel.go
@@ -16,6 +16,7 @@ func (*testModel) GetInferenceParameters() *model.PresetInferenceParam {
 	return &model.PresetInferenceParam{
 		GPUCountRequirement: "1",
 		DeploymentTimeout:   time.Duration(30) * time.Minute,
+		InferenceFile:       "inference-api.py",
 	}
 }
 func (*testModel) SupportDistributedInference() bool {
@@ -28,6 +29,7 @@ func (*testDistributedModel) GetInferenceParameters() *model.PresetInferencePara
 	return &model.PresetInferenceParam{
 		GPUCountRequirement: "1",
 		DeploymentTimeout:   time.Duration(30) * time.Minute,
+		InferenceFile:       "inference-api.py",
 	}
 }
 func (*testDistributedModel) SupportDistributedInference() bool {
