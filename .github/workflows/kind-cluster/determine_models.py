6c0c8ffe4a51f80b823f84ba6eb88107a853f29e
ishaansehgal99 <ishaanforthewin@gmail.com>
2024-01-22 14:29:42 -0800

    feat: add determine models pipeline

---
+++ .github/workflows/kind-cluster/determine_models.py
@@ -0,0 +1,124 @@
+import json
+import os
+import shutil
+import subprocess
+import uuid
+from pathlib import Path
+
+import yaml
+
+
+def read_yaml(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = yaml.safe_load(file)
+            return data
+    except (IOError, yaml.YAMLError) as e:
+        print(f"Error reading {file_path}: {e}")
+        return None
+
+supp_models_yaml = 'presets/models/supported_models.yaml'
+YAML_PR = read_yaml(supp_models_yaml)
+# Format: {falcon-7b : {model_name:falcon-7b, type:text-generation, revision: #, tag: #}}
+MODELS = {model['name']: model for model in YAML_PR['models']}
+KAITO_REPO_URL = "https://github.com/Azure/kaito.git"
+
+def set_multiline_output(name, value):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        delimiter = uuid.uuid1()
+        print(f'{name}<<{delimiter}', file=fh)
+        print(value, file=fh)
+        print(delimiter, file=fh)
+
+def create_matrix(models_list):
+    """Create GitHub Matrix"""
+    return json.dumps({"model": models_list})
+
+def run_command(command):
+    """Execute a shell command and return the output."""
+    try:
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, 
+                                   stderr=subprocess.PIPE, shell=True)
+        output, error = process.communicate()
+        if process.returncode != 0:
+            print(f"Error: {error.decode('utf-8')}")
+            return None
+        return output.decode('utf-8').strip()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+def get_yaml_from_branch(branch, file_path):
+    """Read YAML from a branch"""
+    subprocess.run(['git', 'fetch', 'origin', branch], check=True)
+    subprocess.run(['git', 'checkout', 'origin/' + branch], check=True)
+    return read_yaml(file_path)
+
+def detect_changes_in_yaml(yaml_main, yaml_pr): 
+    """Detecting relevant changes in support_models.yaml"""
+    models_to_build = []
+    for model_pr in yaml_pr:
+        # Searches for matching models
+        model_main = next((m for m in yaml_main if m['name'] == model_pr['name']), None)
+        # New Model
+        if not model_main: 
+            models_to_build.append(model_pr['name'])
+        # Model Version Update
+        elif model_pr.get('version') != model_main.get('version'):
+            models_to_build.append(model_pr['name'])
+        # Model Tag Update
+        elif model_pr.get('tag') != model_main.get('tag'):
+            models_to_build.append(model_pr['name'])
+    return models_to_build
+
+def models_to_build(files_changed):
+    """Models to build based on changed files."""
+    models = set()
+    if supp_models_yaml in files_changed:
+        yaml_main = get_yaml_from_branch('main', supp_models_yaml)
+        models.update(detect_changes_in_yaml(yaml_main, YAML_PR))
+    for model, model_info in MODELS.items():
+        if any(file.startswith(f'presets/models/{model_info["type"]}') for file in files_changed):
+            models.add(model)
+    return list(models)
+
+def check_modified_models(pr_branch):
+    """Check for modified models in the repository."""
+    repo_dir = Path.cwd() / "repo"
+
+    if repo_dir.exists():
+        shutil.rmtree(repo_dir)
+
+    run_command(f"git clone {KAITO_REPO_URL} {repo_dir}")
+    os.chdir(repo_dir)
+
+    run_command("git checkout --detach")
+    run_command("git fetch origin main:main")
+    run_command(f"git fetch origin {pr_branch}:{pr_branch}")
+    run_command(f"git checkout {pr_branch}")
+
+    files = run_command("git diff --name-only origin/main")
+    os.chdir(Path.cwd().parent)
+
+    modified_models = models_to_build(files)
+    
+    print("Modified Models (Images to build): ", modified_models)
+
+    return modified_models
+
+
+def main():
+    pr_branch = os.environ.get("PR_BRANCH", "main")
+    # Logic to determine affected models
+    # Example: affected_models = ['model1', 'model2', 'model3']
+    affected_models = check_modified_models(pr_branch)
+
+    # Convert the list of models into JSON matrix format
+    matrix = create_matrix(affected_models)
+
+    # Set the matrix as an output for the GitHub Actions workflow
+    set_multiline_output('matrix', matrix)
+
+if __name__ == "__main__":
+    main()
+
