a6653443c4e8751ae32b5141aab732d8dac67659
ishaansehgal99 <ishaanforthewin@gmail.com>
2024-02-07 21:46:59 -0800

    fix: update training api - loraconfig

--- presets/training/cli.py
+++ presets/training/cli.py
@@ -1,20 +1,46 @@
-
 from dataclasses import asdict, dataclass, field
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Literal, Optional, Union
 
 import torch
 from peft import LoraConfig
-# import transformers
 from transformers import BitsAndBytesConfig
 
-DEFAULT_LORA_CONFIG = LoraConfig(
-    r=16,
-    lora_alpha=32,
-    target_modules=["query_key_value"],
-    lora_dropout=0.05,
-    bias="none",
-    task_type="CAUSAL_LM"
-)
+# DEFAULT_LORA_CONFIG = LoraConfig(
+#     r=16,
+#     lora_alpha=32,
+#     target_modules=["query_key_value"],
+#     lora_dropout=0.05,
+#     bias="none",
+#     task_type="CAUSAL_LM"
+# )
+
+# TODO: Need to fix this up to allow multiple types for these fields as intended
+@dataclass
+class ExtLoraConfig(LoraConfig):
+    init_lora_weights: bool = field(default=True, metadata={"help": "Enable initialization of LoRA weights"})
+    layers_to_transform: Optional[List[int]] = field(default=None, metadata={"help": "Layer indices to apply LoRA"})
+    layers_pattern: Optional[List[str]] = field(default=None, metadata={"help": "Pattern to match layers for LoRA"})
+    loftq_config: Dict[str, any] = field(default_factory=dict, metadata={"help": "LoftQ configuration for quantization"}) 
+#     r: int = field(default=8, metadata={"help": "Lora attention dimension"})
+#     target_modules: Optional[Union[List[str], str]] = field(
+#         default=None,
+#         metadata={"help": "List of module names or regex expressions to replace with LoRA"}
+#     )
+#     task_type: Optional[str] = field(default=None, metadata={"help": "Megatron core module"})
+#     lora_alpha: int = field(default=8, metadata={"help": "Lora alpha scaling factor"})
+#     lora_dropout: float = field(default=0.0, metadata={"help": "Dropout rate for LoRA layers"})
+#     fan_in_fan_out: bool = field(default=False, metadata={"help": "Set true if layer weights are (fan_in, fan_out)"})
+#     bias: Literal["none", "all", "lora_only"] = field(default="none", metadata={"help": "Bias type for LoRA layers"})
+#     use_rslora: bool = field(default=False, metadata={"help": "Use Rank-Stabilized LoRA"})
+#     modules_to_save: Optional[List[str]] = field(default_factory=list, metadata={"help": "Modules to save besides LoRA layers"})
+#     # init_lora_weights: Optional[Union[bool, Literal["gaussian", "loftq"]]] = field(default=True, metadata={"help": "Initialize LoRA weights method"})
+#     # layers_to_transform: Optional[Union[List[int], int]] = field(default=None, metadata={"help": "Layer indices to apply LoRA"})
+#     # layers_pattern: Optional[Union[List[str], str]] = field(default=None, metadata={"help": "Pattern to match layers for LoRA"})
+#     rank_pattern: Optional[Dict[str, int]] = field(default_factory=dict, metadata={"help": "Custom ranks for specific layers"})
+#     alpha_pattern: Optional[Dict[str, int]] = field(default_factory=dict, metadata={"help": "Custom alphas for specific layers"})
+#     megatron_config: Optional[Dict[str, any]] = field(default=None, metadata={"help": "Megatron TransformerConfig for parallel layers"})
+#     megatron_core: Optional[str] = field(default="megatron.core", metadata={"help": "Megatron core module"})
+#     # loftq_config: Union[LoftQConfig, Dict[str, any]] = field(default_factory=dict, metadata={"help": "LoftQ configuration for quantization"}) 
 
 @dataclass
 class ModelConfig:
@@ -30,10 +56,10 @@ class ModelConfig:
     proxies: Optional[str] = field(default=None, metadata={"help": "Proxy configuration for downloading the model"})
     output_loading_info: bool = field(default=False, metadata={"help": "Output additional loading information"})
     allow_remote_files: bool = field(default=False, metadata={"help": "Allow using remote files, default is local only"})
-    revision: str = field(default="main", metadata={"help": "Specific model version to use"})
+    m_revision: str = field(default="main", metadata={"help": "Specific model version to use"})
     trust_remote_code: bool = field(default=False, metadata={"help": "Enable trusting remote code when loading the model"})
-    load_in_4bit: bool = field(default=False, metadata={"help": "Load model in 4-bit mode"})
-    load_in_8bit: bool = field(default=False, metadata={"help": "Load model in 8-bit mode"})
+    m_load_in_4bit: bool = field(default=False, metadata={"help": "Load model in 4-bit mode"})
+    m_load_in_8bit: bool = field(default=False, metadata={"help": "Load model in 8-bit mode"})
     torch_dtype: Optional[str] = field(default=None, metadata={"help": "The torch dtype for the pre-trained model"})
     device_map: str = field(default="auto", metadata={"help": "The device map for the pre-trained model"})
 
@@ -67,21 +93,15 @@ class ModelConfig:
         self.torch_dtype = getattr(torch, self.torch_dtype) if self.torch_dtype else None
 
 @dataclass
-class TrainingConfig:
-    use_gradient_checkpointing: bool = field(default=False, metadata={"help": "If True, use gradient ckpting method to save memory at expense of slower backward pass"})
-    lora_config: Optional[LoraConfig] = field(default=DEFAULT_LORA_CONFIG, metadata={"help": "Configuration for LoRA layers"})
-
-
-@dataclass
 class QuantizationConfig(BitsAndBytesConfig):
     quant_method: str = field(default="bitsandbytes", metadata={"help": "Quantization Method {bitsandbytes,gptq,awq}"})
     load_in_8bit: bool = field(default=False, metadata={"help": "Enable 8-bit quantization"})
     load_in_4bit: bool = field(default=False, metadata={"help": "Enable 4-bit quantization"})
     llm_int8_threshold: float = field(default=6.0, metadata={"help": "LLM.int8 threshold"})
-    llm_int8_skip_modules: list = field(default_factory=list, metadata={"help": "Modules to skip for 8-bit conversion"})
+    llm_int8_skip_modules: List[str] = field(default=None, metadata={"help": "Modules to skip for 8-bit conversion"})
     llm_int8_enable_fp32_cpu_offload: bool = field(default=False, metadata={"help": "Enable FP32 CPU offload for 8-bit"})
     llm_int8_has_fp16_weight: bool = field(default=False, metadata={"help": "Use FP16 weights for LLM.int8"})
-    bnb_4bit_compute_dtype: str = field(default="torch.float32", metadata={"help": "Compute dtype for 4-bit quantization"})
+    bnb_4bit_compute_dtype: str = field(default="float32", metadata={"help": "Compute dtype for 4-bit quantization"})
     bnb_4bit_quant_type: str = field(default="fp4", metadata={"help": "Quantization type for 4-bit"})
     bnb_4bit_use_double_quant: bool = field(default=False, metadata={"help": "Use double quantization for 4-bit"})
 
--- presets/training/training-api.py
+++ presets/training/training-api.py
@@ -10,17 +10,18 @@ import torch
 import transformers
 import uvicorn
 from accelerate import Accelerator
-from cli import ModelConfig, QuantizationConfig, TrainingConfig
+from cli import ExtLoraConfig, ModelConfig, QuantizationConfig
 from datasets import load_dataset
 from fastapi import FastAPI, HTTPException
 from peft import (LoraConfig, PeftConfig, get_peft_model,
                   prepare_model_for_kbit_training)
 from pydantic import BaseModel
 from transformers import (AutoConfig, AutoModelForCausalLM, AutoTokenizer,
-                          BitsAndBytesConfig, HfArgumentParser)
+                          BitsAndBytesConfig, HfArgumentParser,
+                          TrainingArguments)
 
-parser = HfArgumentParser((ModelConfig, QuantizationConfig, TrainingConfig))
-model_config, bnb_config, training_config, additional_args = parser.parse_args_into_dataclasses(
+parser = HfArgumentParser((ModelConfig, QuantizationConfig, ExtLoraConfig, TrainingArguments))
+model_config, bnb_config, ext_lora_config, targs, additional_args = parser.parse_args_into_dataclasses(
     return_remaining_strings=True
 )
 
@@ -29,43 +30,44 @@ print("Additional arguments:", additional_args)
 # model_config.process_additional_args(additional_args)
 model_args = asdict(model_config)
 model_args["local_files_only"] = not model_args.pop('allow_remote_files')
+model_args["revision"] = model_args.pop('m_revision')
+model_args["load_in_4bit"] = model_args.pop('m_load_in_4bit')
+model_args["load_in_8bit"] = model_args.pop('m_load_in_8bit')
 
 app = FastAPI()
 accelerator = Accelerator()
 
-# DEFAULT_BNB_CONFIG = BitsAndBytesConfig(
-#     load_in_4bit=True,
-#     bnb_4bit_use_double_quant=True,
-#     bnb_4bit_quant_type="nf4",
-#     bnb_4bit_compute_dtype=torch.bfloat16,
-# )
-
 # Load BitsAndBytesConfig
-# bnb_config_args = asdict(bnb_config)
-# bnb_config = BitsAndBytesConfig(**bnb_config_args)
+bnb_config_args = asdict(bnb_config)
+bnb_config = BitsAndBytesConfig(**bnb_config_args)
+enable_qlora = bnb_config.is_quantizable()
 
 # Load the Pre-Trained Model
 tokenizer = AutoTokenizer.from_pretrained(**model_args)
-
-enable_qlora = bnb_config.is_quantizable()
 model = AutoModelForCausalLM.from_pretrained(
     **model_args,
     quantization_config=bnb_config if enable_qlora else None,
 )
 
-# tokenizer.pad_token = tokenizer.eos_token
+if not tokenizer.pad_token:
+    tokenizer.pad_token = tokenizer.eos_token
 
 if enable_qlora:
     # Preparing the Model for QLoRA
     model = prepare_model_for_kbit_training(model)
 
+assert ext_lora_config is not None, "LoraConfig must be specified"
+lora_config_args = asdict(ext_lora_config)
+# TODO: How to pass dict/lists/other ds on cmd line
+lora_config_args["target_modules"] = ["query_key_value"]
+lora_config = LoraConfig(**lora_config_args)
+print(lora_config)
 
-assert training_config.lora_config is not None, "LoraConfig must be specified in TrainingConfig"
-
-model = get_peft_model(model, training_config.lora_config)
-# model.config.use_cache = False
+model = get_peft_model(model, lora_config)
+model.config.use_cache = False
 model.print_trainable_parameters()
 
+# TODO: How to abstract this into API
 # Loading and Preparing the Dataset 
 def preprocess_data(example):
     prompt = f"<human>: {example['Context']}\n<assistance>: {example['Response']}".strip()
@@ -78,16 +80,32 @@ dataset = dataset.shuffle().map(preprocess_data)
 print("Dataset Dimensions: ", dataset.shape)
 
 # Setting Up the Training Arguments
+# training_args = transformers.TrainingArguments(
+#     # auto_find_batch_size=True, # Auto finds largest batch size that fits into memory
+#     # gradient_checkpointing_kwargs={"use_reentrant": False},
+#     # gradient_checkpointing=False,
+#     # ddp_backend="nccl",
+#     # ddp_find_unused_parameters=False,
+#     per_device_train_batch_size=1,
+#     num_train_epochs=4, # Number of training epochs
+#     learning_rate=2e-4, # lr
+#     bf16=False, # precision
+#     save_total_limit=4, # Total # of ckpts to save
+#     logging_steps=10, # # of steps between each logging
+#     output_dir=".", #  Dir where model ckpts saved
+#     save_strategy='epoch', # Strategy for saving ckpts. Here ckpt saved after each epoch
+# )
+
 training_args = transformers.TrainingArguments(
-    auto_find_batch_size=True, # Auto finds largest batch size that fits into memory
-    # gradient_checkpointing_kwargs={"use_reentrant": False},
-    # gradient_checkpointing=False,
-    # ddp_backend="nccl",
-    # ddp_find_unused_parameters=False,
-    # per_device_train_batch_size=1,
+    # auto_find_batch_size=True, # Auto finds largest batch size that fits into memory
+    gradient_checkpointing_kwargs={"use_reentrant": False},
+    gradient_checkpointing=False,
+    ddp_backend="nccl",
+    ddp_find_unused_parameters=False,
+    per_device_train_batch_size=1,
     num_train_epochs=4, # Number of training epochs
     learning_rate=2e-4, # lr
-    bf16=True, # precision
+    bf16=False, # precision
     save_total_limit=4, # Total # of ckpts to save
     logging_steps=10, # # of steps between each logging
     output_dir=".", #  Dir where model ckpts saved
