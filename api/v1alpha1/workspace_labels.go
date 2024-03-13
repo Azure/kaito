4b3d3ba071393ebca5a5a28fbd02833ddc6cc009
ishaansehgal99 <ishaanforthewin@gmail.com>
2024-03-13 11:05:16 -0700

    feat: sample front end

--- api/v1alpha1/workspace_labels.go
+++ api/v1alpha1/workspace_labels.go
@@ -13,6 +13,9 @@ const (
 	// AnnotationEnableLB determines whether kaito creates LoadBalancer type service for testing.
 	AnnotationEnableLB = KAITOPrefix + "enablelb"
 
+	// AnnotationEnableSampleFrontEnd determines whether kaito creates a sample front end using OSS Chainlit
+	AnnotationEnableSampleFrontEnd = KAITOPrefix + "enable-sample-frontend"
+
 	// LabelWorkspaceName is the label for workspace name.
 	LabelWorkspaceName = KAITOPrefix + "workspace"
 
