5c3fb02789556458c36382e0e3733e699590700b
Smriti Dahal <93288516+smritidahal653@users.noreply.github.com>
2024-04-26 11:09:18 -0700

    feat: [SKU modularization] adding azure sku handler (#360)
    **Reason for Change**:
    Adding implementation for Azure sku handler along with unit test
    
    **Requirements**
    
    - [x] added unit tests and e2e tests (if applicable).
    
    **Issue Fixed**:
    <!-- If this PR fixes GitHub issue 4321, add "Fixes #4321" to the next
    line. -->
    
    **Notes for Reviewers**:

---
+++ pkg/sku/cloud_sku_handler_test.go
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+package sku
+
+import (
+	"testing"
+)
+
+func TestAzureSKUHandler(t *testing.T) {
+	handler := NewAzureSKUHandler()
+
+	// Test GetSupportedSKUs
+	skus := handler.GetSupportedSKUs()
+	if len(skus) == 0 {
+		t.Errorf("GetSupportedSKUs returned an empty array")
+	}
+
+	// Test GetGPUConfigs with a SKU that is supported
+	sku := "Standard_NC6s_v3"
+	configMap := handler.GetGPUConfigs()
+	config, exists := configMap[sku]
+	if !exists {
+		t.Errorf("Supported SKU missing from GPUConfigs")
+	}
+	if config.SKU != sku {
+		t.Errorf("Incorrect config returned for a supported SKU")
+	}
+
+	// Test GetGPUConfigs with a SKU that is not supported
+	sku = "Unsupported_SKU"
+	config, exists = configMap[sku]
+	if exists {
+		t.Errorf("Unsupported SKU found in GPUConfigs")
+	}
+}
