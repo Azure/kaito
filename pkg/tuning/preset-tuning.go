7f77be77a8237fc5885d2160a0e16f468c6bb550
ishaansehgal99 <ishaanforthewin@gmail.com>
2024-04-25 21:54:27 -0700

    feat: Adding image data source, tuning job manifest model param config

--- pkg/tuning/preset-tuning.go
+++ pkg/tuning/preset-tuning.go
@@ -3,6 +3,7 @@ package tuning
 import (
 	"context"
 	"fmt"
+	"k8s.io/apimachinery/pkg/api/resource"
 	"os"
 
 	kaitov1alpha1 "github.com/azure/kaito/api/v1alpha1"
@@ -15,7 +16,35 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func GetInstanceGPUCount(sku string) int {
+const (
+	Port5000   = int32(5000)
+	TuningFile = "fine_tuning_api.py"
+)
+
+var (
+	containerPorts = []corev1.ContainerPort{{
+		ContainerPort: Port5000,
+	}}
+
+	// Come up with valid liveness and readiness probes for fine-tuning
+	// TODO: livenessProbe = &corev1.Probe{}
+	// TODO: readinessProbe = &corev1.Probe{}
+
+	tolerations = []corev1.Toleration{
+		{
+			Effect:   corev1.TaintEffectNoSchedule,
+			Operator: corev1.TolerationOpEqual,
+			Key:      resources.GPUString,
+		},
+		{
+			Effect: corev1.TaintEffectNoSchedule,
+			Value:  resources.GPUString,
+			Key:    "sku",
+		},
+	}
+)
+
+func getInstanceGPUCount(sku string) int {
 	gpuConfig, exists := kaitov1alpha1.SupportedGPUConfigs[sku]
 	if !exists {
 		return 1
@@ -80,6 +109,104 @@ func EnsureTuningConfigMap(ctx context.Context, workspaceObj *kaitov1alpha1.Work
 
 func CreatePresetTuning(ctx context.Context, workspaceObj *kaitov1alpha1.Workspace,
 	tuningObj *model.PresetParam, kubeClient client.Client) (client.Object, error) {
-	// TODO
-	return nil, nil
+	initContainers, imagePullSecrets, volumes, volumeMounts, err := prepareDataSource(ctx, workspaceObj, kubeClient)
+	if err != nil {
+		return nil, err
+	}
+
+	err = EnsureTuningConfigMap(ctx, workspaceObj, tuningObj, kubeClient)
+	if err != nil {
+		return nil, err
+	}
+
+	shmVolume, shmVolumeMount := utils.ConfigSHMVolume(*workspaceObj.Resource.Count)
+	if shmVolume.Name != "" {
+		volumes = append(volumes, shmVolume)
+	}
+	if shmVolumeMount.Name != "" {
+		volumeMounts = append(volumeMounts, shmVolumeMount)
+	}
+
+	cmVolume, cmVolumeMount := utils.ConfigCMVolume(workspaceObj.Tuning.ConfigTemplate)
+	volumes = append(volumes, cmVolume)
+	volumeMounts = append(volumeMounts, cmVolumeMount)
+
+	modelCommand, err := prepareModelRunParameters(ctx, tuningObj)
+	if err != nil {
+		return nil, err
+	}
+	commands, resourceReq := prepareTuningParameters(ctx, workspaceObj, modelCommand, tuningObj)
+	tuningImage := GetTuningImageInfo(ctx, workspaceObj, tuningObj)
+
+	jobObj := resources.GenerateTuningJobManifest(ctx, workspaceObj, tuningImage, imagePullSecrets, *workspaceObj.Resource.Count, commands,
+		containerPorts, nil, nil, resourceReq, tolerations, initContainers, volumes, volumeMounts)
+
+	err = resources.CreateResource(ctx, jobObj, kubeClient)
+	if client.IgnoreAlreadyExists(err) != nil {
+		return nil, err
+	}
+	return jobObj, nil
+}
+
+// Now there are three options for DataSource: 1. URL - 2. HostPath - 3. Image
+func prepareDataSource(ctx context.Context, workspaceObj *kaitov1alpha1.Workspace, kubeClient client.Client) ([]corev1.Container, []corev1.LocalObjectReference, []corev1.Volume, []corev1.VolumeMount, error) {
+	var initContainers []corev1.Container
+	var volumes []corev1.Volume
+	var volumeMounts []corev1.VolumeMount
+	var imagePullSecrets []corev1.LocalObjectReference
+	switch {
+	case workspaceObj.Tuning.Input.Image != "":
+		initContainers, volumes, volumeMounts = handleImageDataSource(ctx, workspaceObj)
+		_, imagePullSecrets = GetDataSrcImageInfo(ctx, workspaceObj)
+	}
+	return initContainers, imagePullSecrets, volumes, volumeMounts, nil
+}
+
+func handleImageDataSource(ctx context.Context, workspaceObj *kaitov1alpha1.Workspace) ([]corev1.Container, []corev1.Volume, []corev1.VolumeMount) {
+	var initContainers []corev1.Container
+	// Constructing a multistep command that lists, copies, and then lists the destination
+	command := "ls -la /data && cp -r /data/* " + utils.DefaultDataVolumePath + " && ls -la " + utils.DefaultDataVolumePath
+	initContainers = append(initContainers, corev1.Container{
+		Name:    "data-extractor",
+		Image:   workspaceObj.Tuning.Input.Image,
+		Command: []string{"sh", "-c", command},
+		VolumeMounts: []corev1.VolumeMount{
+			{
+				Name:      "data-volume",
+				MountPath: utils.DefaultDataVolumePath,
+			},
+		},
+	})
+
+	volumes, volumeMounts := utils.ConfigDataVolume("")
+	return initContainers, volumes, volumeMounts
+}
+
+func prepareModelRunParameters(ctx context.Context, tuningObj *model.PresetParam) (string, error) {
+	modelCommand := utils.BuildCmdStr(TuningFile, tuningObj.ModelRunParams)
+	return modelCommand, nil
+}
+
+// prepareTuningParameters builds a PyTorch command:
+// accelerate launch <TORCH_PARAMS> baseCommand <MODEL_PARAMS>
+// and sets the GPU resources required for tuning.
+// Returns the command and resource configuration.
+func prepareTuningParameters(ctx context.Context, wObj *kaitov1alpha1.Workspace, modelCommand string, tuningObj *model.PresetParam) ([]string, corev1.ResourceRequirements) {
+	// Set # of processes to GPU Count
+	numProcesses := getInstanceGPUCount(wObj.Resource.InstanceType)
+	tuningObj.TorchRunParams["num_processes"] = fmt.Sprintf("%d", numProcesses)
+	torchCommand := utils.BuildCmdStr(tuningObj.BaseCommand, tuningObj.TorchRunParams)
+	torchCommand = utils.BuildCmdStr(torchCommand, tuningObj.TorchRunRdzvParams)
+	commands := utils.ShellCmd(torchCommand + " " + modelCommand)
+
+	resourceRequirements := corev1.ResourceRequirements{
+		Requests: corev1.ResourceList{
+			corev1.ResourceName(resources.CapacityNvidiaGPU): resource.MustParse(tuningObj.GPUCountRequirement),
+		},
+		Limits: corev1.ResourceList{
+			corev1.ResourceName(resources.CapacityNvidiaGPU): resource.MustParse(tuningObj.GPUCountRequirement),
+		},
+	}
+
+	return commands, resourceRequirements
 }
