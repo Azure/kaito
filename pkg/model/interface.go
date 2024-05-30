c3ef9402381c8e0d726f419e18473b9d659fa015
ishaansehgal99 <ishaanforthewin@gmail.com>
2024-05-29 21:48:52 -0700

    update field names

--- pkg/model/interface.go
+++ pkg/model/interface.go
@@ -15,23 +15,21 @@ type Model interface {
 
 // PresetParam defines the preset inference parameters for a model.
 type PresetParam struct {
-	ModelFamilyName           string            // The name of the model family.
-	ImageAccessMode           string            // Defines where the Image is Public or Private.
-	DiskStorageRequirement    string            // Disk storage requirements for the model.
-	GPUCountRequirement       string            // Number of GPUs required for the Preset.
-	TotalGPUMemoryRequirement string            // Total GPU memory required for the Preset.
-	PerGPUMemoryRequirement   string            // GPU memory required per GPU.
-	TuningMethodGPUMemory     map[string]int    // Min GPU memory per tuning method (batch size 1).
-	TorchRunParams            map[string]string // Parameters for configuring the torchrun command.
-	TorchRunRdzvParams        map[string]string // Optional rendezvous parameters for distributed training/inference using torchrun (elastic).
-	// BaseCommand is the initial command (e.g., 'torchrun', 'accelerate launch') used in the command line.
-	BaseCommand    string
-	ModelRunParams map[string]string // Parameters for running the model training/inference.
+	ModelFamilyName               string            // The name of the model family.
+	ImageAccessMode               string            // Defines where the Image is Public or Private.
+	DiskStorageRequirement        string            // Disk storage requirements for the model.
+	GPUCountRequirement           string            // Number of GPUs required for the Preset. Used for inference.
+	TotalGPUMemoryRequirement     string            // Total GPU memory required for the Preset. Used for inference.
+	PerGPUMemoryRequirement       string            // GPU memory required per GPU. Used for inference.
+	TuningPerGPUMemoryRequirement map[string]int    // Min GPU memory per tuning method (batch size 1). Used for tuning.
+	TorchRunParams                map[string]string // Parameters for configuring the torchrun command.
+	TorchRunRdzvParams            map[string]string // Optional rendezvous parameters for distributed training/inference using torchrun (elastic).
+	BaseCommand                   string            // The initial command (e.g., 'torchrun', 'accelerate launch') used in the command line.
+	ModelRunParams                map[string]string // Parameters for running the model training/inference.
 	// ReadinessTimeout defines the maximum duration for creating the workload.
 	// This timeout accommodates the size of the image, ensuring pull completion
 	// even under slower network conditions or unforeseen delays.
 	ReadinessTimeout time.Duration
-	// WorldSize defines the number of processes required for distributed inference.
-	WorldSize int
-	Tag       string // The model image tag
+	WorldSize        int    // Defines the number of processes required for distributed inference.
+	Tag              string // The model image tag
 }
