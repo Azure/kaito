655b850792dd1536762e79d5112e9e8a76647289
ishaansehgal99 <ishaanforthewin@gmail.com>
2024-06-24 13:12:20 -0400

    fix: remove push secret

--- test/e2e/utils/utils.go
+++ test/e2e/utils/utils.go
@@ -206,8 +206,7 @@ func GenerateTuningWorkspaceManifest(name, namespace, registry, imageName, e2eOu
 		URLs: []string{ExampleDatasetURL},
 	}
 	workspace.Tuning.Output = &kaitov1alpha1.DataDestination{
-		Image:           fmt.Sprintf("%s/adapter-falcon-7b-e2e-test:%s", registry, e2eOutputImageTag),
-		ImagePushSecret: imagePullSecret[0],
+		Image: fmt.Sprintf("%s/adapter-falcon-7b-e2e-test:%s", registry, e2eOutputImageTag),
 	}
 
 	workspace.Tuning.Config = customConfigMapName
