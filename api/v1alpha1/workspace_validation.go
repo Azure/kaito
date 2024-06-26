82c18655526af9c886da87d6acd70d2dee7e1e6c
ishaansehgal99 <ishaanforthewin@gmail.com>
2024-06-26 19:14:14 -0400

    fix: nil pointer exception

--- api/v1alpha1/workspace_validation.go
+++ api/v1alpha1/workspace_validation.go
@@ -45,7 +45,7 @@ func (w *Workspace) Validate(ctx context.Context) (errs *apis.FieldError) {
 		errs = errs.Also(w.validateCreate().ViaField("spec"))
 		if w.Inference != nil {
 			// TODO: Add Adapter Spec Validation - Including DataSource Validation for Adapter
-			errs = errs.Also(w.Resource.validateCreate(*w.Inference, *w.Tuning).ViaField("resource"),
+			errs = errs.Also(w.Resource.validateCreate(w.Inference, w.Tuning).ViaField("resource"),
 				w.Inference.validateCreate().ViaField("inference"))
 		}
 		if w.Tuning != nil {
@@ -299,19 +299,19 @@ func (r *DataDestination) validateUpdate(old *DataDestination) (errs *apis.Field
 	return errs
 }
 
-func (r *ResourceSpec) validateCreate(inference InferenceSpec, tuning TuningSpec) (errs *apis.FieldError) {
+func (r *ResourceSpec) validateCreate(inference *InferenceSpec, tuning *TuningSpec) (errs *apis.FieldError) {
 	var presetName string
-	if inference.Preset != nil {
+	if inference != nil {
 		presetName = strings.ToLower(string(inference.Preset.Name))
 	}
-	if tuning.Preset != nil && *r.Count > 1 {
+	if tuning != nil && *r.Count > 1 {
 		errs = errs.Also(apis.ErrInvalidValue("Tuning does not currently support multinode configurations. Please set the node count to 1. Future support with DeepSpeed will allow this.", "count"))
 	}
 	instanceType := string(r.InstanceType)
 
 	// Check if instancetype exists in our SKUs map
 	if skuConfig, exists := SupportedGPUConfigs[instanceType]; exists {
-		if inference.Preset != nil {
+		if inference != nil {
 			model := plugin.KaitoModelRegister.MustGet(presetName) // InferenceSpec has been validated so the name is valid.
 			// Validate GPU count for given SKU
 			machineCount := *r.Count
