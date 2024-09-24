d82897d8419ed3e9f2fcf9fdafc45867030a7fcf
ishaansehgal99 <ishaanforthewin@gmail.com>
2024-09-24 11:44:18 -0700

    fix: Add function for getting embedding dim
    Signed-off-by: ishaansehgal99 <ishaanforthewin@gmail.com>

--- presets/rag_service/embedding/huggingface_local.py
+++ presets/rag_service/embedding/huggingface_local.py
@@ -8,4 +8,13 @@ class LocalHuggingFaceEmbedding(BaseEmbeddingModel):
         self.model = HuggingFaceEmbedding(model_name=model_name)
 
     def get_text_embedding(self, text: str):
+        """Returns the text embedding for a given input string."""
         return self.model.get_text_embedding(text)
+
+    def get_embedding_dimension(self) -> int:
+        """Infers the embedding dimension by making a local call to get the embedding of a dummy text."""
+        dummy_input = "This is a dummy sentence."
+        embedding = self.get_text_embedding(dummy_input)
+        
+        # TODO Assume embedding is a 1D array (needs to be tested); return its length (the dimension size)
+        return len(embedding)
--- presets/rag_service/embedding/huggingface_remote.py
+++ presets/rag_service/embedding/huggingface_remote.py
@@ -9,4 +9,13 @@ class RemoteHuggingFaceEmbedding(BaseEmbeddingModel):
         self.model = HuggingFaceInferenceAPIEmbedding(model_name=model_name, api_key=api_key)
 
     def get_text_embedding(self, text: str):
+        """Returns the text embedding for a given input string."""
         return self.model.get_text_embedding(text)
+    
+    def get_embedding_dimension(self) -> int:
+        """Infers the embedding dimension by making a remote call to get the embedding of a dummy text."""
+        dummy_input = "This is a dummy sentence."
+        embedding = self.get_text_embedding(dummy_input)
+        
+        # TODO Assume embedding is a 1D array (needs to be tested); return its length (the dimension size)
+        return len(embedding)
