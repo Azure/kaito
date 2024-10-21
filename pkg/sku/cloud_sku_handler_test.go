f932ea11ce3126e1ea2cdbde9ffc0c64d8ed35c6
qfai <quke@microsoft.com>
2024-10-21 15:27:08 +0800

    add sku tester

--- pkg/sku/cloud_sku_handler_test.go
+++ pkg/sku/cloud_sku_handler_test.go
@@ -62,3 +62,30 @@ func TestAwsSKUHandler(t *testing.T) {
 		t.Errorf("Unsupported SKU found in GPUConfigs")
 	}
 }
+
+func TestAksArcSKUHandler(t *testing.T) {
+	handler := NewAksArcSKUHandler()
+
+	skus := handler.GetSupportedSKUs()
+	if len(skus) == 0 {
+		t.Errorf("GetSupportedSKUs returned an empty array")
+	}
+
+	// Test GetGPUConfigs with a SKU that is supported
+	sku := "MOCVirtualMachine"
+	configMap := handler.GetGPUConfigs()
+	config, exists := configMap[sku]
+	if !exists {
+		t.Errorf("Supported SKU missing from GPUConfigs")
+	}
+	if config.SKU != sku {
+		t.Errorf("Incorrect config returned for a supported SKU")
+	}
+
+	// Test GetGPUConfigs with a SKU that is not supported
+	sku = "Unsupported_SKU"
+	config, exists = configMap[sku]
+	if exists {
+		t.Errorf("Unsupported SKU found in GPUConfigs")
+	}
+}
