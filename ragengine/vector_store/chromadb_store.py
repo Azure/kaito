3cd3c5e49a60764aa5c1d1bcd99cb6ca1651dc92
ishaansehgal99 <ishaanforthewin@gmail.com>
2024-10-23 14:32:02 -0700

    feat: Abstract Vector Store

--- ragengine/vector_store/chromadb_store.py
+++ ragengine/vector_store/chromadb_store.py
@@ -1,16 +1,16 @@
 from typing import List
 from ragengine.models import Document
 
-import faiss
-from llama_index.vector_stores.faiss import FaissVectorStore
+import chromadb
+from llama_index.vector_stores.chroma import ChromaVectorStore
 from .base import BaseVectorStore
 
-class FaissVectorStoreHandler(BaseVectorStore):
+class ChromaDBVectorStoreHandler(BaseVectorStore):
     def __init__(self, embedding_manager):
         super().__init__(embedding_manager)
-        self.dimension = self.embedding_manager.get_embedding_dimension()
+        self.chroma_client = chromadb.EphemeralClient()
 
     def _create_new_index(self, index_name: str, documents: List[Document]) -> List[str]:
-        faiss_index = faiss.IndexFlatL2(self.dimension)
-        vector_store = FaissVectorStore(faiss_index=faiss_index)
+        chroma_collection = self.chroma_client.create_collection(index_name)
+        vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
         return self._create_index_common(index_name, documents, vector_store)
