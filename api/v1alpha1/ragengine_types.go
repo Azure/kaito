788c32c4d3820535d28430f25db7c04a3b59c1a2
Bangqi Zhu <bangqizhu@microsoft.com>
2024-10-23 02:05:03 -0700

    add printcolumn
    Signed-off-by: Bangqi Zhu <bangqizhu@microsoft.com>

--- api/v1alpha1/ragengine_types.go
+++ api/v1alpha1/ragengine_types.go
@@ -91,6 +91,9 @@ type RAGEngineStatus struct {
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:path=ragengines,scope=Namespaced,categories=ragengine
 // +kubebuilder:storageversion
+// +kubebuilder:printcolumn:name="Instance",type="string",JSONPath=".spec.compute.instanceType",description=""
+// +kubebuilder:printcolumn:name="ResourceReady",type="string",JSONPath=".status.conditions[?(@.type==\"ResourceReady\")].status",description=""
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp",description=""
 type RAGEngine struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
