e88634619ffc40f23c2aec3423de7b07ad3e0c3a
Fei Guo <guofei@microsoft.com>
2024-11-06 16:49:36 -0800

    chore: restruct workspace controller code - part 1 (#675)
    We will move workspace related code in a central package /pkg/workspace/
    for better code readability.
    This change starts the efforts by moving the controller and inference
    directories.

--- presets/models/phi3/model.go
+++ presets/models/phi3/model.go
@@ -6,9 +6,9 @@ import (
 	"time"
 
 	kaitov1alpha1 "github.com/kaito-project/kaito/api/v1alpha1"
-	"github.com/kaito-project/kaito/pkg/inference"
 	"github.com/kaito-project/kaito/pkg/model"
 	"github.com/kaito-project/kaito/pkg/utils/plugin"
+	"github.com/kaito-project/kaito/pkg/workspace/inference"
 )
 
 func init() {
